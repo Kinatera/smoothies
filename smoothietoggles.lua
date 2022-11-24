c8278c04198b429d3159d617d9ab55b22916bcd6
Kinatera <117174531+Kinatera@users.noreply.github.com>
2022-11-24 06:23:16 +0300

    +code
    added smoothie kits section

--- smoothietoggles.lua
+++ smoothietoggles.lua
@@ -16,9 +16,63 @@ local Window = Rayfield:CreateWindow({
         },
 	KeySystem = false,
 })
-local Tab1 = Window:CreateTab("Offense Smoothies", 4483362458) -- Title, Image
-local Section = Tab1:CreateSection("You must have the Blender equipped which is a part of the Smoothie Boy class to use any of these below.\nDouble click to splash the smoothies on enemies.\nYou cannot have 2 auto smoothies enabled at once.")
-local Toggle0t1 = Tab1:CreateToggle({
+local Tab1 = Window:CreateTab("Smoothie Kits", 4483362458) -- Title, Image
+
+local Section = Tab1:CreateSection("You must have the Blender equipped which is a part of the Smoothie Boy class to use any of these below.\nDouble click to splash the smoothies on enemies.")
+
+local Button = Tab1:CreateButton({
+	Name = "Buff/Support Kit (PP, SSB, BB)",
+	Callback = function()
+				game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Angry")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Resist")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Healing")
+	end,
+})
+local Button = Tab1:CreateButton({
+	Name = "Tank Kit (SSB, SSB, BB)",
+	Callback = function()
+				game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Resist")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Resist")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Healing")
+	end,
+})
+local Button = Tab1:CreateButton({
+	Name = "Self-Defense Kit (HB, CoI, BB)",
+	Callback = function()
+				game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Damage")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Ice")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Healing")
+	end,
+})
+local Button = Tab1:CreateButton({
+	Name = "Trolling Kit (CoI, SB, UwU)",
+	Callback = function()
+				game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Ice")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Slowmo")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Furry")
+	end,
+})
+local Button = Tab1:CreateButton({
+	Name = "Anti-Furry Kit (CoW, SB, CoI)",
+	Callback = function()
+				game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Water")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Slowmo")
+                wait(2.6)
+                game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Ice")
+	end,
+})
+local Tab2 = Window:CreateTab("Offense Smoothies", 4483362458) -- Title, Image
+local Section = Tab2:CreateSection("You must have the Blender equipped which is a part of the Smoothie Boy class to use any of these below.\nDouble click to splash the smoothies on enemies.\nYou cannot have 2 auto smoothies enabled at once.")
+local Toggle0t2 = Tab2:CreateToggle({
 	Name = "Auto Power Punch (Increases damage dealt by self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -32,7 +86,7 @@ local Toggle0t1 = Tab1:CreateToggle({
         end
 	end,
 })
-local Toggle1t1 = Tab1:CreateToggle({
+local Toggle1t2 = Tab2:CreateToggle({
 	Name = "Auto Harsh Berry (Damages self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -46,7 +100,7 @@ local Toggle1t1 = Tab1:CreateToggle({
         end
 	end,
 })
-local Toggle2t1 = Tab1:CreateToggle({
+local Toggle2t1 = Tab2:CreateToggle({
 	Name = "Auto Cup Of Ice (Freezes self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -60,7 +114,7 @@ local Toggle2t1 = Tab1:CreateToggle({
         end
 	end,
 })
-local Toggle3t1 = Tab1:CreateToggle({
+local Toggle3t1 = Tab2:CreateToggle({
 	Name = "Auto Cup Of Water (Wets self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -74,9 +128,9 @@ local Toggle3t1 = Tab1:CreateToggle({
         end
 	end,
 })
-local Tab2 = Window:CreateTab("Defense Smoothies", 4483362458) -- Title, Image
-local Section = Tab2:CreateSection("You must have the Blender equipped which is a part of the Smoothie Boy class to use any of these below.\nDouble click to splash the smoothies on enemies.\nYou cannot have 2 auto smoothies enabled at once.")
-local Toggle0t2 = Tab2:CreateToggle({
+local Tab3 = Window:CreateTab("Defense Smoothies", 4483362458) -- Title, Image
+local Section = Tab3:CreateSection("You must have the Blender equipped which is a part of the Smoothie Boy class to use any of these below.\nDouble click to splash the smoothies on enemies.\nYou cannot have 2 auto smoothies enabled at once.")
+local Toggle0t3 = Tab3:CreateToggle({
 	Name = "Auto Benevolent Banana (Heals self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -90,7 +144,7 @@ local Toggle0t2 = Tab2:CreateToggle({
         end
 	end,
 })
-local Toggle1t2 = Tab2:CreateToggle({
+local Toggle1t3 = Tab3:CreateToggle({
 	Name = "Auto Shielded S-B (Increases defense for self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -104,7 +158,7 @@ local Toggle1t2 = Tab2:CreateToggle({
         end
 	end,
 })
-local Toggle2t2 = Tab2:CreateToggle({
+local Toggle2t3 = Tab3:CreateToggle({
 	Name = "Auto IFrame Banana (Gives forcefield to self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -118,9 +172,9 @@ local Toggle2t2 = Tab2:CreateToggle({
         end
 	end,
 })
-local Tab3 = Window:CreateTab("Misc Smoothies", 4483362458) -- Title, Image
-local Section = Tab3:CreateSection("You must have the Blender equipped which is a part of the Smoothie Boy class to use any of these below.\nDouble click to splash the smoothies on enemies.\nYou cannot have 2 auto smoothies enabled at once.")
-local Toggle0t3 = Tab3:CreateToggle({
+local Tab4 = Window:CreateTab("Misc Smoothies", 4483362458) -- Title, Image
+local Section = Tab4:CreateSection("You must have the Blender equipped which is a part of the Smoothie Boy class to use any of these below.\nDouble click to splash the smoothies on enemies.\nYou cannot have 2 auto smoothies enabled at once.")
+local Toggle0t3 = Tab4:CreateToggle({
 	Name = "Auto Sending Strawberry (Teleports self/enemy to a random nearby area)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -134,7 +188,7 @@ local Toggle0t3 = Tab3:CreateToggle({
         end
 	end,
 })
-local Toggle1t3 = Tab3:CreateToggle({
+local Toggle1t4 = Tab4:CreateToggle({
 	Name = "Auto Slowberry (Slows self/enemy)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -148,7 +202,7 @@ local Toggle1t3 = Tab3:CreateToggle({
         end
 	end,
 })
-local Toggle2t3 = Tab3:CreateToggle({
+local Toggle2t4 = Tab4:CreateToggle({
 	Name = "Auto SmUwUthie (Makes self/enemies text become furry-like)",
 	CurrentValue = false,
 	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
@@ -162,8 +216,8 @@ local Toggle2t3 = Tab3:CreateToggle({
         end
 	end,
 })
-local Tab4 = Window:CreateTab("Keybinds", 4483362458) -- Title, Image
-local Keybind1 = Tab4:CreateKeybind({
+local Tab5 = Window:CreateTab("Keybinds", 4483362458) -- Title, Image
+local Keybind1 = Tab5:CreateKeybind({
 	Name = "Power Punch",
 	CurrentKeybind = "KeypadOne",
 	HoldToInteract = false,
@@ -172,7 +226,7 @@ local Keybind1 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Angry")
 	end,
 })
-local Keybind2 = Tab4:CreateKeybind({
+local Keybind2 = Tab5:CreateKeybind({
 	Name = "Benevolent Banana",
 	CurrentKeybind = "KeypadTwo",
 	HoldToInteract = false,
@@ -181,7 +235,7 @@ local Keybind2 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Healing")
 	end,
 })
-local Keybind3 = Tab4:CreateKeybind({
+local Keybind3 = Tab5:CreateKeybind({
 	Name = "Shielded S-B",
 	CurrentKeybind = "KeypadThree",
 	HoldToInteract = false,
@@ -190,7 +244,7 @@ local Keybind3 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Resist")
 	end,
 })
-local Keybind4 = Tab4:CreateKeybind({
+local Keybind4 = Tab5:CreateKeybind({
 	Name = "Cup Of Ice",
 	CurrentKeybind = "KeypadFour",
 	HoldToInteract = false,
@@ -199,7 +253,7 @@ local Keybind4 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Ice")
 	end,
 })
-local Keybind5 = Tab4:CreateKeybind({
+local Keybind5 = Tab5:CreateKeybind({
 	Name = "IFrame Banana",
 	CurrentKeybind = "KeypadFive",
 	HoldToInteract = false,
@@ -208,7 +262,7 @@ local Keybind5 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Forcefield")
 	end,
 })
-local Keybind6 = Tab4:CreateKeybind({
+local Keybind6 = Tab5:CreateKeybind({
 	Name = "Slowberry",
 	CurrentKeybind = "KeypadSix",
 	HoldToInteract = false,
@@ -217,7 +271,7 @@ local Keybind6 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Slowmo")
 	end,
 })
-local Keybind7 = Tab4:CreateKeybind({
+local Keybind7 = Tab5:CreateKeybind({
 	Name = "Sending Strawberry",
 	CurrentKeybind = "KeypadSeven",
 	HoldToInteract = false,
@@ -226,7 +280,7 @@ local Keybind7 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Teleport")
 	end,
 })
-local Keybind8 = Tab4:CreateKeybind({
+local Keybind8 = Tab5:CreateKeybind({
 	Name = "Harsh Berry",
 	CurrentKeybind = "KeypadEight",
 	HoldToInteract = false,
@@ -235,7 +289,7 @@ local Keybind8 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Damage")
 	end,
 })
-local Keybind9 = Tab4:CreateKeybind({
+local Keybind9 = Tab5:CreateKeybind({
 	Name = "Cup Of Water",
 	CurrentKeybind = "KeypadNine",
 	HoldToInteract = false,
@@ -244,7 +298,7 @@ local Keybind9 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Water")
 	end,
 })
-local Keybind10 = Tab4:CreateKeybind({
+local Keybind10 = Tab5:CreateKeybind({
 	Name = "SmUwUthie",
 	CurrentKeybind = "KeypadPlus",
 	HoldToInteract = false,
@@ -253,10 +307,10 @@ local Keybind10 = Tab4:CreateKeybind({
 		game:GetService("Players").LocalPlayer.Character.Blender.Create:FireServer("Furry")
 	end,
 })
-local Tab5 = Window:CreateTab("Credits", 4483362458) -- Title, Image
-local Label = Tab5:CreateLabel("Credits: Kinatera#7917 @Discord - Rayfield UI Lib at discord.gg/sirius")
-local Paragraph = Tab5:CreateParagraph({Title = "Why I made this:", Content = "I loved playing smoothie boy to meme around with others and do all sorts of things like smoothie stands! Although making the smoothies took way too long, this was why I made this little thing quickly, If you're using it please don't abuse it as I love this game, I'd hate to see smoothie boy abused due to this. Anyway have fun and thanks for reading! - Kinatera"})
-local Button = Tab5:CreateButton({
+local Tab6 = Window:CreateTab("Credits", 4483362458) -- Title, Image
+local Label = Tab6:CreateLabel("Credits: Kinatera#7917 @Discord - Rayfield UI Lib at discord.gg/sirius")
+local Paragraph = Tab6:CreateParagraph({Title = "Why I made this:", Content = "I loved playing smoothie boy to meme around with others and do all sorts of things like smoothie stands! Although making the smoothies took way too long, this was why I made this little thing quickly, If you're using it please don't abuse it as I love this game, I'd hate to see smoothie boy abused due to this. Anyway have fun and thanks for reading! - Kinatera"})
+local Button = Tab6:CreateButton({
 	Name = "Go back to the loader to change Smoothie Addiction version.",
 	Callback = function()
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinatera/smoothies/main/loader.lua"))()
